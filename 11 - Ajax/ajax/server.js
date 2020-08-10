49ca9ca72a24d73e1983d995789966fba4ebac8c
juvenalbruno <bruno.andrade178@hotmail.com>
2020-08-10 16:38:24 -0300

    10_08_2020

--- 11 - Ajax/ajax/server.js
+++ 11 - Ajax/ajax/server.js
@@ -6,5 +6,44 @@ app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json())
 
-app.get('/teste', (req, res) => res.send('OK'))
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './upload')
+    },
+    filename: function (req, file, callback) {
+        callback(null, `${Data.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage }).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if(err) {
+            return res.end('Ocorreu um erro.')
+        }
+
+        res.end('Concluídocom sucesso.')
+    })
+})
+
+app.post('/formulario', (req, res) => {
+    res.send({
+        ...req.body,
+        id: 7
+    })
+})
+
+app.get('/parOuImpar', (req, res) => {
+    // req.body
+    // req.query
+    // req.params
+    const par = parseInt(req.query.numero) % 2 == 0
+    res.send({
+        resultado: par ? 'par' : 'impar'
+    })
+})
+
 app.listen(8080, () => console.log('Executando...'))
